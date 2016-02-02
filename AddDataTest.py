f8eb096c7cc138f45f399321c8beece0570b2367
Chuqiaoo <gongchuqiao@gmail.com>
2016-02-02 17:30:43 +0000

    Create add data test script

--- AddDataTest.py
+++
@@ -1,4 +0,0 @@
-__author__ = 'chuqiao'
-import script
-script.addDataToSolrFromUrl("http://www.elixir-europe.org:8080/events", "http://www.elixir-europe.org:8080/events");
-script.addDataToSolrFromUrl("http://localhost/ep/events?state=published&field_type_tid=All", "http://localhost/ep/events");
