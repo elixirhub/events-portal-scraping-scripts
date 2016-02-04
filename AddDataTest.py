85f23a93daaa21a21b1c1e72cf55cca3fc2f0b4b
Chuqiaoo <gongchuqiao@gmail.com>
2016-02-04 21:10:54 +0000

    rename the script to EventsPortal

--- AddDataTest.py
+++ AddDataTest.py
@@ -1,4 +1,4 @@
 __author__ = 'chuqiao'
-import script
-script.addDataToSolrFromUrl("http://www.elixir-europe.org:8080/events", "http://www.elixir-europe.org:8080/events");
-script.addDataToSolrFromUrl("http://localhost/ep/events?state=published&field_type_tid=All", "http://localhost/ep/events");
+import EventsPortal
+EventsPortal.addDataToSolrFromUrl("http://www.elixir-europe.org:8080/events", "http://www.elixir-europe.org:8080/events");
+EventsPortal.addDataToSolrFromUrl("http://localhost/ep/events?state=published&field_type_tid=All", "http://localhost/ep/events");
