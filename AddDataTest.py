b49840b276e1b6f3726d51f8a633aa346a73a756
Chuqiaoo <gongchuqiao@gmail.com>
2016-04-21 14:53:48 +0100

    change the input url

--- AddDataTest.py
+++ AddDataTest.py
@@ -45,10 +45,10 @@ logger.info('start at %s' % datetime.now())
 
 # EventsPortal.addDataToSolrFromUrl("http://bioevents-portal.org/eventsfull", "http://bioevents-portal.org/events","http://localhost:8983/solr/event_portal");
 
-EventsPortal.addDataToSolrFromUrl("http://bioevents-portal.org/eventsfull/test?state=published&field_type_tid=All", "http://bioevents-portal.org/events","139.162.217.53:8983/solr/eventsportal")
+EventsPortal.addDataToSolrFromUrl("http://bioevents-portal.org/eventsfull/test?state=published&field_type_tid=All", "http://bioevents-portal.org/events","localhost:8983/solr/event_portal")
 logger.info('finish at %s' % datetime.now())
 
 
 if  __name__ == '__main__':
 
-    EventsPortal.addDataToSolrFromUrl("http://bioevents-portal.org/eventsfull/test?state=published&field_type_tid=All", "http://bioevents-portal.org/events","139.162.217.53:8983/solr/eventsportal")
+    EventsPortal.addDataToSolrFromUrl("http://bioevents-portal.org/eventsfull/test?state=published&field_type_tid=All", "http://bioevents-portal.org/events","localhost:8983/solr/event_portal")
