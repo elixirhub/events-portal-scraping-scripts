09b1d9ffab51a414629f01826d1f388b5c2cd6bb
Chuqiaoo <gongchuqiao@gmail.com>
2016-04-26 21:48:18 +0100

    create log file

--- ScheduleAddData.py
+++ ScheduleAddData.py
@@ -5,6 +5,36 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import EventsPortal
 import sys
 
+import logging
+
+
+def logger():
+    """
+       Function that initialises logging system
+    """
+    global logger
+    # create logger with 'syncsolr'
+    logger = logging.getLogger('scheduleAddData')
+    logger.setLevel(logging.DEBUG)
+
+    # specifies the lowest severity that will be dispatched to the appropriate destination
+
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('scheduleAddData.log')
+    # fh.setLevel(logging.WARN)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    # StreamHandler instances send messages to streams
+    # ch.setLevel(logging.DEBUG)
+
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(ch)
+    logger.addHandler(fh)
 
 
 
@@ -23,7 +53,8 @@ def scheduleUpdateSolr(sourceUrl,patternUrl,solrUrl):
             time.sleep(20)
 
     except (KeyboardInterrupt, SystemExit):
-        pass
+        logger.error('Can not schedule add data to solr  \n%s' % str(sys.exc_info()))
+
 
 
 if  __name__ == '__main__':
