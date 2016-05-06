4feb17a2f079943948f9788473532c371078f080
Chuqiaoo <gongchuqiao@gmail.com>
2016-05-06 13:10:36 +0100

    give the inn url as parameter

--- SyncSolr.py
+++ SyncSolr.py
@@ -145,5 +145,5 @@ if __name__ == '__main__':
 
     init(
         "http://139.162.217.53:8983/solr/eventsportal/select?q=*:*&fl=eventId,name,alternateName,startDate,endDate,hostInstitution,description,eventType,keywords,topic,locationName,locationCity,locationCountry,locationPostcode,latitude,longitude,url,&rows=2147483647&wt=csv",
-        "http://iann.pro/solrdev/iann"
+        "http://iann.pro/solr/iann"
     )
